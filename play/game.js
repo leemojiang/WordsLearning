47cdc400710f6baa51c094a852d310c1b15755f8
R9000p <126@126.com>
2025-01-03 21:25:57 +0800

    修改了背单词游戏的逻辑,先是unknown的单词,然后是已知的单词

--- play/game.js
+++ play/game.js
@@ -9,22 +9,18 @@ const unknownButton = document.getElementById('unknown');
 
 document.addEventListener('DOMContentLoaded', function () {
   // 从本地存储获取单词列表
-  chrome.storage.local.get(['wordCounts'], function (result) {
-    let dic = result.wordCounts || {};
-    words = Object.keys(dic)
-    console.log(words)
+  chrome.storage.local.get(['wordCounts', 'unknowCounts'], function (result) {
+    let wc = result.wordCounts || {};
+    let uwc = result.unknowCounts || {}; //unknown word list
+
+    console.log(uwc)
+    console.log(wc)
+
+    let wl = Object.keys(wc).sort((a, b) => wc[b] - wc[a]) //decending sorted word list
+    let uwl = Object.keys(uwc).sort((a, b) => uwc[b] - uwc[a])
+    words = uwl.concat(wl)
     showWord();
   });
-
-  // 显示当前单词
-  function showWord() {
-    if (words.length > 0) {
-      wordContainer.textContent = words[currentIndex];
-      console.log(words[currentIndex])
-    } else {
-      wordContainer.textContent = 'No words available';
-    }
-  }
 
   // 处理按键操作
   knownButton.addEventListener('click', function () {
@@ -34,29 +30,58 @@ document.addEventListener('DOMContentLoaded', function () {
   unknownButton.addEventListener('click', function () {
     updateFrequency(false);
   });
+});
 
-  // 更新单词频率
-  function updateFrequency(isKnown) {
-    if (words.length > 0) {
-      if (isKnown) {
-        words[currentIndex].frequency--;
-      } else {
-        words[currentIndex].frequency++;
-      }
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'Space') { updateFrequency(true); }
+  else if (event.code === 'KeyU') { updateFrequency(false); }
+});
 
-      // 保存更新后的单词列表
-      chrome.storage.local.set({ words: words }, function () {
-        // 显示下一个单词
-        currentIndex = (currentIndex + 1) % words.length;
-        showWord();
-        console.log("Storage update")
-      });
-    }
+
+// 显示当前单词
+function showWord() {
+  if (words.length > 0) {
+    wordContainer.textContent = words[currentIndex];
+    console.log("Show",words[currentIndex]);
+  } else {
+    wordContainer.textContent = 'No words available';
   }
+}
 
-  function updateWords(){
+// 更新单词频率
+function updateFrequency(isKnown) {
+  // 保存更新后的单词列表
+  chrome.storage.local.get({ unknowCounts: {} }, function (result) {
+    let unknowCounts = result.unknowCounts;
+    let word = words[currentIndex];
+
+    console.log("Known word ",word,isKnown)
+
+    if (isKnown) {
+      if (unknowCounts[word]) {
+        unknowCounts[word] -= 1;
+        if (unknowCounts[word] <= 0) {
+          delete unknowCounts[word];
+        }
+
+      }
+    } else {
+      if (unknowCounts[word]) {
+        unknowCounts[word] += 1;
+      } else {
+        unknowCounts[word] = 1;
+      }
+    }
+
+    chrome.storage.local.set({ unknowCounts: unknowCounts }, function () {
+      console.log('Unknown count updated:', unknowCounts);
+    });
+
     // 显示下一个单词
     currentIndex = (currentIndex + 1) % words.length;
     showWord();
-  }
-});
+  });
+
+
+}
+
